f658e53a67ed2e234040eaa98a58e12f157deb61
ArkarPhyo8 <arkarphyo200156@gmail.com>
2024-10-24 17:44:03 +0630

    notification

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,116 @@
-import { View } from 'react-native'
-import React from 'react'
-import TodoHome from '@/src/components/todo/TodoHome'
+import { Alert, Button, Platform, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import TodoHome from "@/src/components/todo/TodoHome";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const Index = () => {
+
+  //push Notification 
+  useEffect(() => {
+    async function configurePushNotifications() {
+      let token;
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+
+      if (finalStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission required",
+          "Push notifications need the appropriate permissions."
+        );
+        return;
+      }
+      try {
+        const projectId =
+          Constants?.expoConfig?.extra?.eas?.projectId ??
+          Constants?.easConfig?.projectId;
+        if (!projectId) {
+          throw new Error("Project ID not found");
+        }
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(token);
+      } catch (e) {
+        token = `${e}`;
+      }
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    }
+    configurePushNotifications();
+  }, []);
+
+
+  //Local Notification
+  useEffect(() => {
+    //send notification to user
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        console.log(notification.request.content.data.userName);
+      }
+    );
+
+    //response data from user
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+        console.log(response.notification.request.content.data.userName);
+      }
+    );
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  function schedulePushNotification() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: "Here is the notification body",
+        data: { userName: "AK" },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
   return (
     <View>
-      <TodoHome/>
+      <View>
+        <TodoHome />
+      </View>
+      <View>
+        <Button
+          title="Press to schedule a notification"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
